659b50db3e2a13760898d7eaaf48bd0f3dd1137f
layandtry <lay_and_try@163.com>
2018-05-29 21:00:37 +0800

    Initial commit 对表的增删改

---
+++ app/src/main/java/com/wlw_wangmiao_2_219/litepaltest/Category.java
@@ -0,0 +1,33 @@
+package com.wlw_wangmiao_2_219.litepaltest;
+
+public class Category {
+    private int id;
+
+    private  String categoryName;
+
+    private int categoryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
